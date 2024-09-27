c81442bc11c6926e9957fc5f80c05d860bb78254
Thomas ☻ <53582981+tthn0@users.noreply.github.com>
2024-09-27 18:43:31 -0500

    Created React Template

---
+++ source/client/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}>Collide Hackathon Yay</header>
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export { App };
