c09c29c3a52dc47f18ed373c6e5b9a090dfb5033
Ashishjob <ashishjob104@gmail.com>
2024-09-28 10:37:04 -0500

    feat: add Loading component

---
+++ source/client/src/components/Loading/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Loading.module.scss';
+
+const Loading = () => {
+  return (
+    <div className={styles.tetrominos}>
+      <div className={`${styles.tetromino} ${styles.box1}`}></div>
+      <div className={`${styles.tetromino} ${styles.box2}`}></div>
+      <div className={`${styles.tetromino} ${styles.box3}`}></div>
+      <div className={`${styles.tetromino} ${styles.box4}`}></div>
+    </div>
+  );
+};
+
+export default Loading;
