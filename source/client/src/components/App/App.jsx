16ae4e10b68ae7e1db53ce68b49bcd68755fbd6e
Thomas ☻ <53582981+tthn0@users.noreply.github.com>
2024-09-29 14:45:51 -0500

    fixed spacing issue on user page

--- source/client/src/components/App/App.jsx
+++ source/client/src/components/App/App.jsx
@@ -5,13 +5,13 @@ import { RecommendedUsers } from "../RecommendedUsers/RecommendedUsers";
 import { CenterColumn } from "../CenterColumn/CenterColumn";
 import { useTrendingTopics } from "../../hooks/useTrendingTopics";
 import styles from "./App.module.scss";
-import { UserCard } from '../Admin/UserCard/UserCard';
-import { useFetchAllUsers } from '../../hooks/useFetchAllUsers'; // Import the custom hook
+import { UserCard } from "../Admin/UserCard/UserCard";
+import { useFetchAllUsers } from "../../hooks/useFetchAllUsers"; // Import the custom hook
 
 function isNumberOrEmpty(variable) {
   return (
-    typeof variable === 'number' || // Check if it's a number
-    variable === ''           // Check if it's an empty string
+    typeof variable === "number" || // Check if it's a number
+    variable === "" // Check if it's an empty string
   );
 }
 
@@ -27,35 +27,32 @@ function App() {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <Header />
-        <div className={styles.gridContainer}>
-          {isNumberOrEmpty(userId) && loading && (
-            <div className={styles.loading}>
-              Loading users...
-            </div>
-          )}
-          {isNumberOrEmpty(userId) && !loading && Array.isArray(users) && users.map(user => (
-            <UserCard key={user.id} user={user} />
-          ))}
-          {error && (
-            <div className={styles.error}>
-              Error: {error}
-            </div>
-          )}
-        </div>
+        {isNumberOrEmpty(userId) && (
+          <div className={styles.gridContainer}>
+            {isNumberOrEmpty(userId) && loading && (
+              <div className={styles.loading}>Loading users...</div>
+            )}
+            {isNumberOrEmpty(userId) &&
+              !loading &&
+              Array.isArray(users) &&
+              users.map((user) => <UserCard key={user.id} user={user} />)}
+            {error && <div className={styles.error}>Error: {error}</div>}
+          </div>
+        )}
         {!isNumberOrEmpty(userId) && (
           <>
-        <div className={styles.divider}></div>
-        <main className={styles.main}>
-          <aside className={styles.left}>
-            <Profile userId={userId} />
-            <RecommendedUsers userId={userId} />
-          </aside>
-          <CenterColumn {...{ userId, trendingTopics }} />
-          <section className={styles.right}>
-            <TrendingTopics trendingTopics={trendingTopics} />
-          </section>
-        </main>
-        </>
+            <div className={styles.divider}></div>
+            <main className={styles.main}>
+              <aside className={styles.left}>
+                <Profile userId={userId} />
+                <RecommendedUsers userId={userId} />
+              </aside>
+              <CenterColumn {...{ userId, trendingTopics }} />
+              <section className={styles.right}>
+                <TrendingTopics trendingTopics={trendingTopics} />
+              </section>
+            </main>
+          </>
         )}
       </div>
     </div>
