7a5d92bf7e67c0a0912f1e2bc3c255b3a9794ba8
Jean Penso <jeanpaulpenso01@live.com>
2024-10-02 22:02:01 -0500

    feat: create init module to set services as a package

---
+++ source/server/services/__init__.py
@@ -0,0 +1,5 @@
+from .user import UserService
+from .users import UsersService
+from .trending_topics import TrendingTopicsService
+
+__all__ = ['UserService', 'UsersService', 'TredingTopicsService']
